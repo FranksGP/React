539bd71ae58869c1fe678d12d44bdc3bdf908f5c
Frank Garzon <fsebastian.garzong@gmail.com>
2026-07-10 11:10:22 -0500

    container, Screen

--- src/MyAwesomeApp.test.tsx
+++ src/MyAwesomeApp.test.tsx
@@ -6,11 +6,27 @@ import { MyAwesomeApp } from './MyAwesomeApp';
 describe('MyAwesomeApp', () => {
 
     test('should render firstName and LastName', () => {
-        render(<MyAwesomeApp />);
-        
-        screen.debug();
+        const { container } = render(<MyAwesomeApp />);
+        // screen.debug();
+        // console.log(container.innerHTML);
 
         const h1 = container.querySelector('h1');
-        expect(h1?.innerHTML).toBe('Fernando');
+        const h3 = container.querySelector('h3');
+
+
+        // console.log(h1);
+        expect(h1?.innerHTML).toContain('Fernando');// toBe sirve para busacar datos primitivos
+        expect(h3?.innerHTML).toContain('Herrera');
+    });
+    test('should render firstName and LastName - screen', () => {
+    render(<MyAwesomeApp />);
+    screen.debug(); // devuelve la estrutura del DOW
+
+    // const h1 = screen.getByRole('heading',{level: 1}); //busca el h1
+    
+    const h1 = screen.getByTestId('first-name-title'); //busca el h1 por el data-testid
+    
+    expect(h1.innerHTML).toContain('Fernando');
+    
     });
 });
