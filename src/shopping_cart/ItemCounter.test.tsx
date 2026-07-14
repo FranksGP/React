53aadf42bbd39483c7458eac2c9c658696c65295
Frank Garzon <fsebastian.garzong@gmail.com>
2026-07-14 15:54:22 -0500

    test events

--- src/shopping_cart/ItemCounter.test.tsx
+++ src/shopping_cart/ItemCounter.test.tsx
@@ -1,14 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import { test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 import { ItemCounter } from "./ItemCounter";
 
-test('itemCounter', () => {
+describe('itemCounter', () => {
 
     test('should render with default values', () => {
+        const name = "Control de Nintendo";
 
-        render(<ItemCounter name="Test item" />)
+        render(<ItemCounter name={name} quantity={undefined}  />)
 
-        screen.debug();
+        
+        expect(screen.getByText(name)).toBeDefined(); // buscar el valor "name" que no este definido
+        // expect(screen.getByText(name)).not.toBeNull();// buscar el balor que no se nulo
+    });
+
+    test('should increase count when +1 button is pressed', () => {
+        render(<ItemCounter name={"test item"} quantity={1} />)
+
+        const [buttonAdd] = screen.getAllByRole('button');
+        fireEvent.click(buttonAdd)
+
+        expect(screen.getByText('2')).toBeDefined();
 
     });
 });
